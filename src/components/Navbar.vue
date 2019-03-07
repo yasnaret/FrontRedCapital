5e44f64e764c3dac33d6929f610863a7c8ff7277
yasnaret <yasnaret.soto@gmail.com>
2019-03-07 15:17:01 -0300

    Started config using CLI v.3

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-toolbar color='blue-grey' dark >
+    <v-toolbar-title > Random Vue-sers</v-toolbar-title>
+    <v-spacer></v-spacer>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import store from './store'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+// axios 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios,axios)
 
 Vue.config.productionTip = false
 
