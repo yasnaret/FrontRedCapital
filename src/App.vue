c978496a81e104f496a29256d81e4e6417b8352c
yasnaret <yasnaret.soto@gmail.com>
2019-03-08 01:14:15 -0300

    Add vuex and vuetify as dependencies.Funcionality it is almost done

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <template>
   <v-app>
     <v-content>
-      <Navbar/>
       <UserCard/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import Navbar from './components/Navbar'
 import UserCard from './components/UserCard'
-
 export default {
   name: 'App',
   components: {
-    Navbar,
-    UserCard
+    UserCard,
   },
   data () {
     return {
--- src/store.js
+++ src/store.js
@@ -1,16 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
+
+let apiURL = "https://randomuser.me/api/?results=1"
 
 export default new Vuex.Store({
   state: {
-
-  },
-  mutations: {
-
+    persons: [],
   },
   actions: {
-
-  }
+    GET_DATA  ({ commit }){
+      axios
+      .get(apiURL)
+      .then(rsp =>rsp.data.results )
+      .then(person=>{
+        commit('reloadNewUser',person)
+      })
+    }
+  },
+  mutations: {
+    reloadNewUser(state,person){
+     state.persons=person
+    }
+   }
 })
